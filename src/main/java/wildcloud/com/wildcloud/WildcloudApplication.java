be9332ad840bd4fc0eaab9c28494d770ccd4d767
Taha-27 <tahashaikh2927@gmail.com>
2025-05-13 21:38:24 +0530

    first file

---
+++ src/main/java/wildcloud/com/wildcloud/WildcloudApplication.java
@@ -0,0 +1,13 @@
+package wildcloud.com.wildcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wildcloud/com/wildcloud/WildcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package wildcloud.com.wildcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
